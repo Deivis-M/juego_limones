f4c202115bff9074af1c86a3c48033781aced674
Deivis-M <nitaballenita09@gmail.com>
2026-06-23 19:25:32 -0500

    mover a la izquierda

--- limones.js
+++ limones.js
@@ -3,6 +3,8 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
+
 function iniciar(){
     dibujarSuelo()
     dibujarPersonaje()
@@ -15,6 +17,21 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="#ec0ba8";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
 }
 
+function moverIzquierda(){
+    personajeX=personajeX-10
+    actualizarPantalla()
+}
+
+function actualizarPantalla(){
+    limpiarCanva()
+    dibujarSuelo()
+    dibujarPersonaje()
+}
+
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
